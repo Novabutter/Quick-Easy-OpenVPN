22b21ac738dcbfef0f2b611c3edeb446ac8a3300
Giovanni <glicameli@outlook.com>
2020-04-21 16:32:00 -0500

    Holy Dickens Update
    I'm not sure what the heck I was thinking, but the script looking further down was woefully broken. Beginning first rounds of testing.

--- vpn-install.sh
+++ vpn-install.sh
@@ -24,7 +24,7 @@ cd ~/OpenVPN/CA
 
 
 echo -e "[ + ] Generating Certificates & Keys"
-read -p '\Country: ' reqCountry
+read -p 'Country: ' reqCountry
 read -p 'State/Province: ' reqState
 read -p 'City: ' reqCity
 read -p 'Organization: ' reqOrganization
@@ -33,19 +33,6 @@ read -p 'Department: ' reqUnit
 
 ## But an if-else loop here for confirming info.
 
-#### THIS DOES NOT WORK. CRREATE YOUR OWN vars FILE
-# sed -i "s/'#set_var EASYRSA_REQ_COUNTRY*'/'set_var EASYRSA_REQ_COUNTRY\t$reqCountry'/g" vars
-# sed -i "s/'#set_var EASYRSA_REQ_PROVINCE*'/'set_var EASYRSA_REQ_PROVINCE\t$reqState'/g" vars
-# sed -i "s/'#set_var EASYRSA_REQ_CITY*'/'set_var EASYRSA_REQ_CITY\t$reqCity'/g" vars
-# sed -i "s/'#set_var EASYRSA_REQ_ORG*'/'set_var EASYRSA_REQ_ORG\t$reqOrganization'/g" vars
-# sed -i "s/'#set_var EASYRSA_REQ_EMAIL*'/'set_var EASYRSA_REQ_EMAIL\t$reqEmail'/g" vars
-# sed -i "s/'#set_var EASYRSA_REQ_OU*'/'set_var EASYRSA_REQ_OU\t$reqUnit'/g" vars
-# echo "if [ -z "$EASYRSA_CALLER" ]; then" > ~/OpenVPN/CA/vars
-# echo "	echo "You appear to be sourcing an Easy-RSA 'vars' file." >&2" >> ~/OpenVPN/CA/vars
-# echo "	echo "This is no longer necessary and is disallowed. See the section called" >&2" >> ~/OpenVPN/CA/vars
-# echo "	echo "'How to use this file' near the top comments for more details." >&2" >> ~/OpenVPN/CA/vars
-# echo "	return 1" >> ~/OpenVPN/CA/vars
-# echo "fi" >> ~/OpenVPN/CA/vars
 echo "set_var EASYRSA_REQ_COUNTRY '$reqCountry'" >> ~/OpenVPN/CA/vars
 echo "set_var EASYRSA_REQ_PROVINCE '$reqState'" >> ~/OpenVPN/CA/vars
 echo "set_var EASYRSA_REQ_CITY '$reqCity'" >> ~/OpenVPN/CA/vars
@@ -54,8 +41,6 @@ echo "set_var EASYRSA_REQ_EMAIL '$reqEmail'" >> ~/OpenVPN/CA/vars
 echo "set_var EASYRSA_REQ_OU '$reqUnit'" >> ~/OpenVPN/CA/vars
 # Create certs and keys
 ./easyrsa init-pki
-## Experementing with piping the expected prompt of a common name
-## Another way to do it if this fails: echo "Y Y N N Y N Y Y N" | ./your_script
 echo "vpn" | ./easyrsa build-ca nopass
 cd ~/OpenVPN/Server
 ./easyrsa init-pki
@@ -75,7 +60,6 @@ sudo cp ~/OpenVPN/Server/pki/dh.pem /etc/openvpn
 mkdir -p ~/client-configs/keys
 chmod -R 700 ~/client-configs ############ BE SURE TO WHEN THE SCRIPT IS DONE chmod 400 this.
 ## Prompt 'How many clients do you have?', then loop that many times. 
-### Another solution would be to see if there is an option for multiple people sharing one client config.
 ####################
 echo "" | ./easyrsa gen-req client1 nopass
 cp ~/OpenVPN/Server/pki/private/client1.key ~/client-configs/keys/
@@ -87,87 +71,145 @@ cp ~/OpenVPN/CA/pki/issued/client1.crt ~/client-configs/keys/
 ####################
 cp ~/OpenVPN/Server/ta.key ~/client-configs/keys/
 sudo cp /etc/openvpn/ca.crt ~/client-configs/keys/
- echo -e "[ + ] Customizing server configuration" ################################################### 4/20/2020 Need to make my own custom file here.
+ echo -e "[ + ] Customizing server configuration" 
 
 
 
 # Customize server.conf file
-# cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
-# gzip -d /etc/openvpn/server.conf.gz
-# sed -i "s/'*tls-auth ta.key 0 # This file is secret'/'tls-auth ta.key 0 # This file is secret'/g" server.conf
-# ## prompt; if port != 1194, then set sed replace port
-# ## prompt tap or tun; if != tun, sed -i "s/*'dev tun'/';dev tun'/g" server.conf sed -i "s/*';dev tap'/'dev tap'/g" server.conf type=tap else type=tun
-# ## prompt tcp or udp; if != udp, sed -i "s/*'proto udp'/';proto udp'/g" server.conf sed -i"s/*';proto tcp'/'proto tcp'/g" server.conf sed -i "s/*'explicit-exit-notify 0'/'explicit-exit-notify 1'/g" server.conf proto=tcp else proto=udp
-# echo "auth SHA256" >> /etc/openvpn/server.conf
-# #echo "duplicate-cn" >> /etc/openvpn/server.conf ########## Prompt user for sharing same config file (security risk)
-# sed -i "^.*dh*/s/*dh*/'dh dh.pem'/g"
-# sed -i "s/*user*/'user nobody'/g"
-# sed -i "s/*group*/'group nogroup'/g"
-# ## prompt force all traffic through secure VPN? if yes sed -i "s/*'push "redirect-gateway def1 bypass-dhcp"'/'push "redirect-gateway def1 bypass-dhcp"'/g" server.conf
-# ### echo 'push "dhcp-option DNS 208.67.222.222"' >> /etc/openvpn/server.conf
-# ###echo 'push "dhcp-option DNS 208.67.220.220"' >> /etc/openvpn/server.conf
-# ## prompt for adding other push networks.
-# ### Need mechanism for determining netmask
-# echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-# # Add iptable/firewall rules
-# INTERFACE="$( ip -o link show | awk '{print $2,$9}' | grep "UP" | cut -d: -f 1 | cut -d@ -f 1)"
-# iptables -A INPUT -i $INTERFACE -m state --state NEW -p $proto --dport $port -j ACCEPT
-# iptables -A INPUT -i $type+ -j ACCEPT
-# iptables -A FORWARD -i $type+ -j 
-# iptables -A FORWARD -i $type+ -o $INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables -A FORWARD -i $INTERFACE -o $type+ -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables-save > /etc/iptables/rules.v4
-# # Start OpenVPN Service
-# echo -e "[ + ] Starting OpenVPN Server"
-# systemctl start openvpn@server && systemctl enable openvpn@server
-# echo -e "[ + ] OpenVPN Server Running!"
-# # Create Client Configuration
-# echo -e "[ + ] Create Client Configs"
-# # How many clients?
-# mkdir -p ~/client-configs/files
-# cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf ~/client-configs/base.conf
-# cd ~/client-configs/base.conf
-# read -p "\nPublic Gateway IP: " ip
-# sed -i "s/my-server-1*/$ip\t$port/g" ~/client-configs/base.conf
-# # if != udp, sed -i "s/*'proto udp'/';proto udp'/g" server.conf sed -i"s/*';proto tcp'/'proto tcp'/g" server.conf sed -i "s/*'explicit-exit-notify 0'/'explicit-exit-notify 1'/g" server.conf
-# sed -i "s/*user*/'user nobody'/g"
-# sed -i "s/*group*/'group nogroup'/g"
-# sed -i "s/*'ca ca.crt'*/'#ca ca.crt'"
-# sed -i "s/*'key client.key'*/'#key client.key'"
-# sed -i "s/*'cert client.crt'*/'#cert client.crt'"
-# sed -i "s/*'tls-auth ta.key 1'*/'#tls-auth ta.key 1'"
-# echo "auth SHA256" >> ~/client-configs/base.conf
-# key-direction 1
-# # if reading "Are there any linux clients that will be connected?". If yes, add the following lines.
-# # script-security 2
-# # up /etc/openvpn/update-resolv-conf
-# # down /etc/openvpn/update-resolv-conf
-# mkdir -p ~/client-configs/make_config.sh
-# mkdir -p ~/client-configs/clients
-# cd ~/client-configs
-# echo "#!/bin/bash" > ~/client-configs/make_config.sh
-# echo "KEY_DIR=~/client-configs/keys" >> ~/client-configs/make_config.sh
-# echo "OUTPUT_DIR=~/client-configs/clients" >> ~/client-configs/make_config.sh
-# echo "BASE_CONFIG=~/client-configs/base.conf" >> ~/client-configs/make_config.sh
-# echo "cat ${BASE_CONFIG} \ " >> ~/client-configs/make_config.sh
-# echo "<(echo -e '<ca>') \ " >> ~/client-configs/make_config.sh
-# echo "${KEY_DIR}/ca.crt \ " >> ~/client-configs/make_config.sh
-# echo "<(echo -e '</ca>\n<cert>') \ " >> ~/client-configs/make_config.sh
-# echo "${KEY_DIR}/${1}.crt \ " >> ~/client-configs/make_config.sh
-# echo "<(echo -e '</cert>\n<key>') \ " >> ~/client-configs/make_config.sh
-# echo "${KEY_DIR}/${1}.key \ " >> ~/client-configs/make_config.sh
-# echo "<(echo -e '</key>\n<tls-auth>') \ " >> ~/client-configs/make_config.sh
-# echo "${KEY_DIR}/ta.key \ " >> ~/client-configs/make_config.sh
-# echo "<(echo -e '</tls-auth>') \ " >> ~/client-configs/make_config.sh
-# echo "> ${OUTPUT_DIR}/${1}.ovpn " >> ~/client-configs/make_config.sh
-# chmod 700 ~/client-configs/make_config.sh
-# cd ~/client-configs
-# # for i amount of times to generate clients, generate client[i]
-# ./make_config.sh client1
-# echo -e "[ * ] VPN client configs generated"
-# echo -e "[ + ] Locking down VPN setup files"
-# chmod -R 400 ~/client-configs ############ Added this line. Take out if problems copying.
-# chmod -R 000 ~/OpenVPN/CA
-# chmod -R 400 ~/OpenVPN/Server
-# cd ~/client-configs/clients
-# echo -e "[ + ] FINISHED! VPN Setup complete!"    
+#sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
+#sudo gzip -d /etc/openvpn/server.conf.gz
+read -p 'What port is the VPN running on? Default is [1194]: ' port
+if [ $port="" ]
+then
+	port="1194"
+fi
+echo "port $port" > ~/OpenVPN/CA/server.conf
+echo "ls-auth ta.key 0 # This file is secret" >> ~/OpenVPN/CA/server.conf
+
+read -p 'TCP or UDP? Default is [udp]: ' protocol
+if [ $protocol="" ]
+then
+	protocol="udp"
+elif [ $protocol="tcp" ||  $protocol="TCP" ] 
+then
+	echo "explicit-exit-notify 1" >> ~/OpenVPN/CA/server.conf
+fi
+echo "proto $protocol" >> ~/OpenVPN/CA/server.conf
+## prompt; 
+### TUN = only traffic, TAP = all traffic
+read -p 'All traffic goes through VPN (tap) or related traffic goes through VPN (tun)? Default is [tun]: ' type
+if [ $type="" ]
+then
+	type="tun"
+fi
+echo "dev $type" >> ~/OpenVPN/CA/server.conf
+echo "ca ca.crt" >> ~/OpenVPN/CA/server.conf
+echo "cert server.crt" >> ~/OpenVPN/CA/server.conf
+echo "key server.key" >> ~/OpenVPN/CA/server.conf
+echo "dh dh.pem" >> ~/OpenVPN/CA/server.conf
+echo "auth SHA256" >> ~/OpenVPN/CA/server.conf
+echo "server 10.8.0.0 255.255.255.0" ## This is a temporary default until you get the network determination in.
+## prompt for universal access via one configuration file (security risk)
+# echo "duplicate-cn" >> server.conf
+echo "ifconfig-pool-persist ipp.txt" >> ~/OpenVPN/CA/server.conf
+echo "user nobody" >> ~/OpenVPN/CA/server.conf
+echo "group nogroup" >> ~/OpenVPN/CA/server.conf
+###############
+read -p 'Reaching to another network? (Y/N): ' pushAnswer
+while [ $pushAnswer="Y" || $pushAnswer="y" ]
+do
+	read -p 'Netmask Address (ex. 192.168.1.0): ' netAddress
+	read -p 'Subnet Mask (ex. 255.255.255.0): ' subMask
+	echo 'push "route $netAddress $subMask"' >> ~/OpenVPN/CA/server.conf
+	echo "Add more networks? (Y/N): " pushAnswer
+done
+echo 'push "dhcp-option DNS 1.1.1.2"' >> ~/OpenVPN/CA/server.conf
+echo 'push "dhcp-option DNS 1.1.1.1"' >> ~/OpenVPN/CA/server.conf
+echo "keepalive 10 120" >> ~/OpenVPN/CA/server.conf
+echo "comp-lzo" >> ~/OpenVPN/CA/server.conf
+echo "persist-key" >> ~/OpenVPN/CA/server.conf
+echo "persist-tun" >> ~/OpenVPN/CA/server.conf
+echo "status openvpn-status.log" >> ~/OpenVPN/CA/server.conf
+echo "verb 3" >> ~/OpenVPN/CA/server.conf
+sudo cp ~/OpenVPN/CA/server.conf /etc/openvpn/
+sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+# Add iptable/firewall rules
+INTERFACE="$( ip -o link show | awk '{print $2,$9}' | grep "UP" | cut -d: -f 1 | cut -d@ -f 1)"
+sudo iptables -A INPUT -i $INTERFACE -m state --state NEW -p $proto --dport $port -j ACCEPT
+sudo iptables -A INPUT -i $type+ -j ACCEPT
+sudo iptables -A FORWARD -i $type+ -j 
+sudo iptables -A FORWARD -i $type+ -o $INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i $INTERFACE -o $type+ -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables-save > /etc/iptables/rules.v4
+# Start OpenVPN Service
+echo -e "[ + ] Starting OpenVPN Server"
+sudo systemctl start openvpn@server && sudo systemctl enable openvpn@server
+echo -e "[ + ] OpenVPN Server Running!"
+# Create Client Configuration
+echo -e "[ + ] Create Client Configs"
+# How many clients?
+mkdir -p ~/client-configs/files
+#cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf ~/client-configs/base.conf
+cd ~/client-configs/
+read -p "Public Gateway IP: " ip
+echo "client" > ~/client-configs/base.conf
+echo "$dev $type" >> ~/client-configs/base.conf
+echo "proto $protcol" >> ~/client-configs/base.conf
+echo "remote $ip $port" >> ~/client-configs/base.conf
+echo "resolv-retry-infinite" >> ~/client-configs/base.conf
+echo "nobind" >> ~/client-configs/base.conf
+echo "user nobody" >> ~/client-configs/base.conf
+echo "group nogroup" >> ~/client-configs/base.conf
+echo "persist-key" >> ~/client-configs/base.conf
+echo "persist-tun" >> ~/client-configs/base.conf
+echo "ca ca.crt" >> ~/client-configs/base.conf
+echo "cert client.crt" >> ~/client-configs/base.conf
+echo "cert client.key" >> ~/client-configs/base.conf
+echo "tls-auth ta.key 1" >> ~/client-configs/base.conf
+echo "auth SHA256" >> ~/client-configs/base.conf
+echo "remote-cert-tls server" >> ~/client-configs/base.conf
+echo "comp-lzo" >> ~/client-configs/base.conf
+echo "verb 3" >> ~/client-configs/base.conf
+echo "key-direction 1" >> ~/client-configs/base.conf
+if [ $protocol="tcp" ] 
+then
+	echo "explicit-exit-notify 1" >> ~/client-configs/base.conf
+fi
+# if reading "Are there any linux clients that will be connected?". If yes, add the following lines.
+# script-security 2
+# up /etc/openvpn/update-resolv-conf
+# down /etc/openvpn/update-resolv-conf
+mkdir -p ~/client-configs/make_config.sh
+mkdir -p ~/client-configs/clients
+cd ~/client-configs
+echo "#!/bin/bash" > ~/client-configs/make_config.sh
+echo "KEY_DIR=~/client-configs/keys" >> ~/client-configs/make_config.sh
+echo "OUTPUT_DIR=~/client-configs/clients" >> ~/client-configs/make_config.sh
+echo "BASE_CONFIG=~/client-configs/base.conf" >> ~/client-configs/make_config.sh
+echo "cat ${BASE_CONFIG} \ " >> ~/client-configs/make_config.sh
+echo "<(echo -e '<ca>') \ " >> ~/client-configs/make_config.sh
+echo "${KEY_DIR}/ca.crt \ " >> ~/client-configs/make_config.sh
+echo "<(echo -e '</ca>\n<cert>') \ " >> ~/client-configs/make_config.sh
+echo "${KEY_DIR}/${1}.crt \ " >> ~/client-configs/make_config.sh
+echo "<(echo -e '</cert>\n<key>') \ " >> ~/client-configs/make_config.sh
+echo "${KEY_DIR}/${1}.key \ " >> ~/client-configs/make_config.sh
+echo "<(echo -e '</key>\n<tls-auth>') \ " >> ~/client-configs/make_config.sh
+echo "${KEY_DIR}/ta.key \ " >> ~/client-configs/make_config.sh
+echo "<(echo -e '</tls-auth>') \ " >> ~/client-configs/make_config.sh
+echo "> ${OUTPUT_DIR}/${1}.ovpn " >> ~/client-configs/make_config.sh
+chmod 700 ~/client-configs/make_config.sh
+cd ~/client-configs
+read -p 'How many individuals will need their own unique connection file?: ' numClients
+for i in {0..$numClients}
+do
+	./make_config.sh client$i
+done
+echo -e "[ * ] VPN client configs generated"
+cp ~/client-configs/clients/client*.ovpn ~/Desktop/
+echo -e "[ + ] Locking down VPN setup files"
+# chattr too
+chmod -R 400 ~/client-configs ############ Added this line. Take out if problems copying.
+chmod -R 000 ~/OpenVPN/CA
+chmod -R 400 ~/OpenVPN/Server
+cd ~/client-configs/clients/
+echo -e "[ + ] FINISHED! VPN Setup complete!"    
